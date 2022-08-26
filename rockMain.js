99fa812788df23063ff58d906f6ddd4b6ebc9970
ValorousPete <ValorousPete@gmail.com>
2022-08-25 21:15:17 -0600

    first function

--- rockMain.js
+++ rockMain.js
@@ -1,5 +1,19 @@
 const prompt = require('prompt-sync')({sigint: true});
-const Math = require('mathjs');
-
-const name = prompt('What is your name? ');
+/*
+const name = prompt('Welcome to ROCK, PAPPER, SISSORS /n Enter you');
 console.log(`Hey there ${name}`);
+*/
+
+function randoomString(){
+    const randNum = Math.floor(Math.random() * 3);
+    switch (randNum) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Sissors';
+        case 2:
+            return 'Paperr';
+    }
+}
+
+console.log(randoomString());
