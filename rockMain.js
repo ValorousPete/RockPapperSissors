58a2ea3134a6829e46d5707e51e4bfe4a9328676
zjcrumpton <Zaccary.Crumpton@charter.com>
2022-08-25 21:19:17 -0600

    comment todos

--- rockMain.js
+++ rockMain.js
@@ -4,10 +4,17 @@ const name = prompt('Welcome to ROCK, PAPPER, SISSORS /n Enter you');
 console.log(`Hey there ${name}`);
 */
 
+// write a function called getComputerChoice, not randoomString
 function randoomString(){
     const randNum = Math.floor(Math.random() * 3);
     switch (randNum) {
         case 0:
+          // using strings like this is called a "magic string"
+          // this is a code smell
+          // instead use constants
+          // the reason for this is when you go to compare this to something later
+          // then you'll be typing the string out by hand again
+          // and might make typos
             return 'Rock';
         case 1:
             return 'Sissors';
